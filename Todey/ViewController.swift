a3911776592da81b4113630e26d157055662d32f
Alaattin Bedir <alaattinbedir@hotmail.com>
2018-07-05 11:16:38 +0300

    Initial Commit

---
+++ Todey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todey
+//
+//  Created by Alaattin Bedir on 5.07.2018.
+//  Copyright © 2018 Magiclamp Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
